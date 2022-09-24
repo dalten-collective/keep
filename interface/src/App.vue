995adca82b127fc80c4c8ad3ddf22c55e85e8dfc
vinney cavallo <vcavallo@gmail.com>
2022-09-24 13:18:14 -0400

    Add pending UI

--- interface/src/App.vue
+++ interface/src/App.vue
@@ -1,57 +1,66 @@
 <template>
   <v-app class="tw-font-silom">
-    <v-app-bar extend color="primary">
-      <template v-slot:prepend>
-        <v-app-bar-nav-icon
-          @click="navDrawer = !navDrawer"
-        ></v-app-bar-nav-icon>
-      </template>
-
-      <v-app-bar-title>
-        <router-link :to="{ name: 'work' }">Keep</router-link>
-      </v-app-bar-title>
-
-      <template v-slot:append>
-        <a
-          href="https://quartus.co"
-          target="_blank"
-          style="position: relative; top: 25px"
-        >
-          <div style="position: relative">
-            <v-img :src="logo" contain height="75" width="75" />
-          </div>
-        </a>
-      </template>
-    </v-app-bar>
-
-    <v-navigation-drawer v-model="navDrawer" bottom temporary color="primary">
-      <v-list color="primary">
-        <v-list-item
-          color="info"
-          v-for="i in items"
-          :key="i.name"
-          :to="{ name: i.route }"
-        >
-          {{ i.name }}
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
-
-    <v-main class="tw-container tw-mx-auto tw-my-8">
-      <router-view></router-view>
-      <!--
-      <button @click="startMainAirlock">Start Main airlock(s)</button>
-      <button @click="closeMainAirlock">Close keep airlock</button>
-      <button @click="closeAgentAirlocks">Close all agent airlocks</button>
+    <div class="tw-flex tw-flex-row tw-justify-center tw-p-8">
       <div>
-        messages:
-        <ul>
-          <li v-for="m in messages" :key="m">{{ m }}</li>
-        </ul>
+        <h1 class="tw-text-2xl">
+        Coming soon. Check back in a few days
+        </h1>
       </div>
-      -->
-    </v-main>
-    <Message style="position: fixed; bottom: 0;" class="tw-fixed tw-bottom-0 tw-right-0 tw-w-full tw-p-2 tw-opacity-100 sm:tw-w-1/3"/>
+    </div>
+    <div v-if="false">
+      <v-app-bar extend color="primary">
+        <template v-slot:prepend>
+          <v-app-bar-nav-icon
+            @click="navDrawer = !navDrawer"
+          ></v-app-bar-nav-icon>
+        </template>
+
+        <v-app-bar-title>
+          <router-link :to="{ name: 'work' }">Keep</router-link>
+        </v-app-bar-title>
+
+        <template v-slot:append>
+          <a
+            href="https://quartus.co"
+            target="_blank"
+            style="position: relative; top: 25px"
+          >
+            <div style="position: relative">
+              <v-img :src="logo" contain height="75" width="75" />
+            </div>
+          </a>
+        </template>
+      </v-app-bar>
+
+      <v-navigation-drawer v-model="navDrawer" bottom temporary color="primary">
+        <v-list color="primary">
+          <v-list-item
+            color="info"
+            v-for="i in items"
+            :key="i.name"
+            :to="{ name: i.route }"
+          >
+            {{ i.name }}
+          </v-list-item>
+        </v-list>
+      </v-navigation-drawer>
+
+      <v-main class="tw-container tw-mx-auto tw-my-8">
+        <router-view></router-view>
+        <!--
+        <button @click="startMainAirlock">Start Main airlock(s)</button>
+        <button @click="closeMainAirlock">Close keep airlock</button>
+        <button @click="closeAgentAirlocks">Close all agent airlocks</button>
+        <div>
+          messages:
+          <ul>
+            <li v-for="m in messages" :key="m">{{ m }}</li>
+          </ul>
+        </div>
+        -->
+      </v-main>
+      <Message style="position: fixed; bottom: 0;" class="tw-fixed tw-bottom-0 tw-right-0 tw-w-full tw-p-2 tw-opacity-100 sm:tw-w-1/3"/>
+    </div>
   </v-app>
 </template>
 
